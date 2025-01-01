9155069d2ebc15f9b5d50e5752dfa2dcef750a06
Felipe Hoch <hochfelipe@gmail.com>
2025-01-01 17:49:45 -0300

    Adding multiple tapes to second order gradients

--- gradient_tape.py
+++ gradient_tape.py
@@ -8,10 +8,22 @@ with tf.GradientTape() as tape:
 
 gradient = tape.gradient(result, input_var)
 
-print(gradient)
-
 # No código acima, iniciamos uma tape que irá observar as operações aplicada na variável pré definida. 
 # Dentro do escopo foi aplicado a operação de extrair o quadrado dela, sendo $x^2$, como sabemos, a derivada para essa operação é $f'(x) = 2.x$, 
 # a mágica ocorre pois a tape registra as derivadas geradas através dos cálculos aplicados na variável, 
 # dessa forma,quando apontamos para a tape e chamamos o método gradient, passando o result que representa a função que estamos derivando e o input,
 # como resultado final temos a taxa de variação.
+
+# Extraindo gradientes de segunda ordem.
+time = tf.Variable(0.)
+
+with tf.GradientTape() as outer_tape:
+    with tf.GradientTape() as inner_tape:
+        position = 4.9 * time ** 2
+    speed = inner_tape.gradient(position, time)
+
+    print(speed)
+
+acceleration = outer_tape.gradient(speed, time)
+
+print(acceleration)
