b633d9437b54d0cdf2feb5174ce82d99aa029781
Felipe Hoch <hochfelipe@gmail.com>
2025-02-23 17:41:50 -0300

    Finish support ticket ranker model

--- support_ticket_ranker.py
+++ support_ticket_ranker.py
@@ -1,5 +1,6 @@
 import keras
-from keras import layers,activations
+from keras import layers, activations, optimizers, losses, metrics
+import numpy as np
 
 vocabulary_size = 10000
 
@@ -33,5 +34,54 @@ model = keras.Model(
 
 print(model.summary())
 
+# Generating synthetical data.
+num_samples = 1280
 
+title_data = np.random.randint(0, 2, size=(num_samples, vocabulary_size))
 
+text_body_data = np.random.randint(0, 2, size=(num_samples, vocabulary_size))
+
+tags_data = np.random.randint(0, 2, size=(num_samples, num_tags))
+
+priority_data = np.random.random(size=(num_samples, 1))
+
+department_data = np.random.randint(0, 2, size=(num_samples, num_departaments))
+
+# Compiling model.
+model.compile(
+    optimizer=keras.optimizers.RMSprop(),
+    loss=[losses.mean_squared_error, losses.categorical_crossentropy],
+    metrics=[[metrics.mean_absolute_error], [metrics.Accuracy]]
+)
+
+model.fit(
+    [title_data, text_body_data, tags_data],
+    [priority_data, department_data],
+    epochs=1
+)
+
+model.evaluate(
+    [title_data, text_body_data, tags_data],
+    [priority_data, department_data]    
+)
+
+priority_preds, department_preds = model.predict(
+    [title_data, text_body_data, tags_data]
+)
+
+# Plotting the neural network architeture.
+keras.utils.plot_model(model, "support_ticket_ranker.png")
+
+keras.utils.plot_model(model, "support_ticket_ranker_with_shapes.png", show_shapes=True)
+
+# Reusing layers on new models.
+features = model.layers[4].output
+
+difficulty = layers.Dense(3, activation=activations.softmax, name="difficulty")(features)
+
+new_model = keras.Model(
+    inputs=[title, text_body, tags],
+    outputs=[priority, department, difficulty]
+)
+
+keras.utils.plot_model(new_model, "support_ticket_ranker_with_difficulty_output.png", show_shapes=True)
